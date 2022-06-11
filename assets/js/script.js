8a9657063eabc57d67fd863f2f96a23c48b4829d
MosNes <7hekarl@gmail.com>
2022-06-11 14:42:08 -0400

    refactored getWeather into a single function with 2 API calls, added formatInput function for testing

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,33 +2,43 @@
 
 const apiKey = "4cab09c32f1d271efdfae18ead81ae10";
 var todaysDate = luxon.DateTime.now();
+var searchInput = $("#city-input")
 
 
 
 //---------------------------FUNCTIONS---------------------------------------------
 
-// returns the latitude and longitude for the city based on the search string
-var getGeoCoding = () => {
-    fetch('http://api.openweathermap.org/geo/1.0/direct?q=Charlotte,nc,us&limit=1&appid='+apiKey)
-    .then(function(response) {
-        return response.json();
+// returns the forecast for the chosen city
+var getWeather = async () => {
+    //returns the latidude and longitude of the inputted city
+    await fetch('http://api.openweathermap.org/geo/1.0/direct?q=Charlotte,nc,us&limit=1&appid='+apiKey)
+    //parses the JSON response
+    .then(async function(response) {
+        let responseJSON = await response.json();
+        return responseJSON;
     })
-    .then(function(data) {
-        console.log(data);
-    })
-};
+    //gets the latitude and longitude, then uses it to make the API call to get the forecast
+    .then(async function(data) {
+        var latitude = data[0].lat;
+        var longitude =data[0].lon;
+        console.log("lat",latitude);
+        console.log("long",longitude)
+        await fetch('https://api.openweathermap.org/data/2.5/onecall?lat='+latitude+'&lon='+longitude+'&exclude=minutely,hourly,alerts&appid='+apiKey)
+        //parses the JSON response
+        .then(async function(response) {
+            let responseJSON = await response.json();
+            console.log(responseJSON);
+            return responseJSON;
+        })
+      
+      });
+    
+    };
 
-
-//returns weather conditions for the current day and a 7 day forecast
-var getWeather = () => {
-    //query string = q={city name},{country code}, {state code} eg q=Charlotte,us,nc
-    fetch('https://api.openweathermap.org/data/2.5/weather?q=Charlotte,us,nc&appid='+apiKey)
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(data) {
-        console.log(data);
-    })
+var formatInput = () => {
+    console.log(searchInput.text());
 };
 
 //---------------------------INITIALIZATIONS---------------------------------------
+
+$("#submit-button").on("click",formatInput);

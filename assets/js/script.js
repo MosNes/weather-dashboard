d58431a836b02cb4a8df5ff58a14e98864516869
MosNes <7hekarl@gmail.com>
2022-06-12 11:12:56 -0400

    added functions to store and load search history to localStorage

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,12 +4,32 @@ const apiKey = "4cab09c32f1d271efdfae18ead81ae10";
 var todaysDate = luxon.DateTime.now();
 const searchInputEl = $("#city-input");
 const currentWeatherContainerEl = $("#current-weather-card");
-const forecastContainerEl= $("#forecast-card-container");
-var searchHistory = {};
+const forecastContainerEl = $("#forecast-card-container");
+var searchHistory = [];
 
 
 //---------------------------FUNCTIONS---------------------------------------------
-var displayCurrentWeather = (data,city) => {
+var storeSearchHistory = () => {
+    //convert the searchHistory array into a JSON string
+    let searchHistoryString = JSON.stringify(searchHistory);
+    //store it in localStorage as "weatherSearchHistory"
+    localStorage.setItem("weatherSearchHistory",searchHistoryString);
+};
+
+var loadSearchHistory = () => {
+    //get stored search history
+    let storedSearchHistory = localStorage.getItem("weatherSearchHistory");
+    //if search history is not present or an empty value, stop
+    if (!storedSearchHistory) {
+        return;
+    }
+    //otherwise parse the value into an array and update the value of searchHistory
+    else {
+        searchHistory = JSON.parse(storedSearchHistory);
+    }
+};
+
+var displayCurrentWeather = (data, city) => {
 
     //clears current card if present
     currentWeatherContainerEl.empty();
@@ -20,17 +40,17 @@ var displayCurrentWeather = (data,city) => {
     let cardTitleEl = $("<h3>").addClass("card-title").text(city);
     let iconContainerEl = $("<p>").addClass("card-text mb-2");
     //the @2x in the URI returns a larger icon for this larger card
-    let iconEl = $("<img>").attr("src","http://openweathermap.org/img/wn/"+data.weather[0].icon+"@2x.png")
+    let iconEl = $("<img>").attr("src", "http://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png")
     let cardSubtitleEl = $("<h5>").addClass("card-subtitle mb-2 text-muted").text(luxon.DateTime.fromSeconds(parseInt(data.dt)).toLocaleString(luxon.DateTime.DATE_SHORT));
-    let tempEl = $("<p>").addClass("card-text mb-2").text("Temp: "+data.temp);
-    let windEl = $("<p>").addClass("card-text mb-2").text("Wind: "+data.wind_speed+" mph");
-    let humidityEl = $("<p>").addClass("card-text mb-2").text("Humidity: "+data.humidity+" %");
+    let tempEl = $("<p>").addClass("card-text mb-2").text("Temp: " + data.temp);
+    let windEl = $("<p>").addClass("card-text mb-2").text("Wind: " + data.wind_speed + " mph");
+    let humidityEl = $("<p>").addClass("card-text mb-2").text("Humidity: " + data.humidity + " %");
     let uvEl = $("<p>").addClass("card-text mb-2").text("UV Index: ");
     let uvPillEl = $("<span>").addClass("badge").text(data.uvi);
-    
+
     //gets the UV Index as a number and changes the color of the uvPillEl depending on the index's severity
     let uvIndex = parseInt(data.uvi);
-    if(uvIndex < 2){
+    if (uvIndex < 2) {
         uvPillEl.addClass("bg-success");
     }
     else if (uvIndex > 6) {
@@ -44,7 +64,7 @@ var displayCurrentWeather = (data,city) => {
     uvEl.append(uvPillEl);
     iconContainerEl.append(iconEl);
     //jQuery can append multiple items in one call
-    cardBodyEl.append(cardTitleEl,cardSubtitleEl,iconContainerEl,tempEl,windEl,humidityEl,uvEl);
+    cardBodyEl.append(cardTitleEl, cardSubtitleEl, iconContainerEl, tempEl, windEl, humidityEl, uvEl);
     cardEl.append(cardBodyEl);
     currentWeatherContainerEl.append(cardEl);
 
@@ -55,19 +75,19 @@ var displayForecastCard = (data) => {
     let cardEl = $("<div>").addClass("card bg-dark col-2 mb-4");
     let cardBodyEl = $("<div>").addClass("card-body");
     let cardTitleEl = $("<h4>").addClass("card-title text-light")
-    //this weather API uses UNIX strings for its datetime values .fromSeconds(parseInt(data.dt)) converts the UNIX string into a more traditional datetime object
-    .text(luxon.DateTime.fromSeconds(parseInt(data.dt)).toLocaleString(luxon.DateTime.DATE_SHORT));
+        //this weather API uses UNIX strings for its datetime values .fromSeconds(parseInt(data.dt)) converts the UNIX string into a more traditional datetime object
+        .text(luxon.DateTime.fromSeconds(parseInt(data.dt)).toLocaleString(luxon.DateTime.DATE_SHORT));
     let iconContainerEl = $("<p>").addClass("card-text mb-2");
     //removed the @2x from this URI to return a smaller Icon for these smaller cards
-    let iconEl = $("<img>").attr("src","http://openweathermap.org/img/wn/"+data.weather[0].icon+".png");
+    let iconEl = $("<img>").attr("src", "http://openweathermap.org/img/wn/" + data.weather[0].icon + ".png");
     //added .day to get the daytime temp
-    let tempEl = $("<p>").addClass("card-text text-light mb-2").text("Temp: "+data.temp.day);
-    let windEl = $("<p>").addClass("card-text text-light mb-2").text("Wind: "+data.wind_speed+" mph");
-    let humidityEl = $("<p>").addClass("card-text text-light mb-2").text("Humidity: "+data.humidity+" %");
+    let tempEl = $("<p>").addClass("card-text text-light mb-2").text("Temp: " + data.temp.day);
+    let windEl = $("<p>").addClass("card-text text-light mb-2").text("Wind: " + data.wind_speed + " mph");
+    let humidityEl = $("<p>").addClass("card-text text-light mb-2").text("Humidity: " + data.humidity + " %");
 
     //add elements to DOM
     iconContainerEl.append(iconEl);
-    cardBodyEl.append(cardTitleEl,iconContainerEl,tempEl,windEl,humidityEl,)
+    cardBodyEl.append(cardTitleEl, iconContainerEl, tempEl, windEl, humidityEl,)
     cardEl.append(cardBodyEl);
     forecastContainerEl.append(cardEl);
 };
@@ -112,29 +132,26 @@ var getWeather = async (city, stateCode) => {
                     let currentWeather = data.current;
                     //capitalizes the first letter of the city
                     let capitalizedCity = city => {
-                        return city.charAt(0).toUpperCase()+city.slice(1);
+                        return city.charAt(0).toUpperCase() + city.slice(1);
                     }
                     displayCurrentWeather(currentWeather, capitalizedCity(city));
                     let forecastArray = data.daily;
 
                     //removes any existing cards from the forecast container
                     forecastContainerEl.empty();
-                    
-                    //constructs forecast cards to display current weather
-                    for (var i = 0; i < 5; i++){
-                        displayForecastCard(forecastArray[i]);
-                        console.log("run"+i);
-                    }
-                    
 
-                  
+                    //constructs forecast cards to display current weather
+                    for (var i = 0; i < 5; i++) {
+                        displayForecastCard(forecastArray[i]);
+                        console.log("run" + i);
+                    }
+
+
+
 
                 })
         })
 };
-
-
-//gets the latitude and longitude, then uses it to make the API call to get the forecast
 
 
 var submitHandler = () => {
@@ -153,6 +170,18 @@ var submitHandler = () => {
         window.alert("Please use the state abbreviation, e.g. 'NC' or 'NY'");
         return;
     }
+
+    //creates object to store the search
+    let searchHistoryObj = {
+        city: city,
+        state: stateCode
+    };
+
+    //push the search history object to the search history array
+    searchHistory.push(searchHistoryObj);
+
+    //call the localStorage function to store the new data
+    storeSearchHistory();
 
     //run the getWeather function using the input
     getWeather(city, stateCode);
